1e480eec3d5145263ac1fdb12f72616316508047
RoxanaCS <roxale.cares@gmail.com>
2018-02-26 10:10:00 -0300

    componentes

---
+++ src/components/Global/Content.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './css/Content.css';
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="Content">
+        <h1>contenido de ejemplo</h1>
+        <p>Parrafo de ejemplo</p>
+      </div>
+    );
+  }
+}
+
+export default Content;
