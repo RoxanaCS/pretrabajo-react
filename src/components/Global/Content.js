a433dc7f8bf5245a029e545ec3698c96f9f3b16c
RoxanaCS <roxale.cares@gmail.com>
2018-02-26 18:57:46 -0300

    mostrar productos en navegador

--- src/components/Global/Content.js
+++ src/components/Global/Content.js
@@ -7,14 +7,7 @@ class Content extends Component {
   render() {
     return (
       <div className="Content">
-        <h1>contenido de ejemplo</h1>
-        <p>Parrafo de ejemplo</p>
-        <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
-          <input type="hidden" name="cmd" value="_s-xclick" />
-          <input type="hidden" name="hosted_button_id" value="TKQLJ4VXZNKJ2" />
-          <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
-          <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-        </form>
+        <h1>Lista de productos disponibles</h1>
         <MercadoLibreApp />
       </div>
     );
--- src/components/Global/MercadoLibreApp.js
+++ src/components/Global/MercadoLibreApp.js
@@ -1,28 +1,39 @@
 import React, {Component} from 'react'
+import './css/Mercado.css';
 
 class MercadoLibreApp extends Component {
 	constructor() {
     	super();
         this.state = { pictures: [],
 				};
+
     }
-componentDidMount() {
-    fetch('https://api.mercadolibre.com/sites/MLU/search?q=chromecast')
+	componentDidMount() {
+    fetch('https://api.mercadolibre.com/sites/MLU/search?q=Computación')
       .then(response => {
         return response.json();
-      }).then(data => {
+      })
+			.then(data => {
 				// se muestran todos los productos en results
 				console.log(data);
 				let pictures = data.results.map((pic) => {
 				return(
-					<div key = {pic.results}>
-						<img src= {pic.thumbnail} />
+					<div class="product" key = {pic.results}>
+						<img class="imgProduct" src= {pic.thumbnail} />
+						<p class="titleProduct">{pic.title}</p>
+						<p>${pic.price}</p>
+						<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
+		          <input type="hidden" name="cmd" value="_s-xclick" />
+		          <input type="hidden" name="hosted_button_id" value="TKQLJ4VXZNKJ2" />
+		          <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
+		          <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+		        </form>
 					</div>
 				)
 			})
         this.setState({ pictures: pictures });
 				// se muestran solo las imagenes contenidas en thumbnail
-				// console.log("state", this.state.pictures);
+				console.log(this.state.pictures);
       })
 		}
 	render() {
